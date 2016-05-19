97b197ede141be40e38bb429ae2cdd9d53eaeaee
jakmeier <jakmeier@ethz.ch>
2016-05-19 16:18:30 +0200

    initial commit

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * An example application you may want to analyze to test your analysis.
+ *
+ */
+public class Test1 {
+    public static void foo() {
+	//int b = 0;	
+	//if(2!=3) {b =1;  }
+        PrinterArray pa = new PrinterArray(5);
+        pa.sendJob(2);
+    }
+}
